c44d0a16e2bd253df0d52ae5ad546ec2faf3e932
arturovilla <rturovilla@tamu.edu>
2021-03-09 15:40:31 -0600

    Create BounceGame.java
    Here is our main file:
    i think it will be easier to put all our methods inside this class

---
+++ BounceGame.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/*
+team members: Arturo Villalobos
+Date: 3-8-21
+Assignment: group project 1
+*/
+
+/*Instructions: Refer to the instructions on Canvas.
+*/
+
+class BounceBattle
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
+
